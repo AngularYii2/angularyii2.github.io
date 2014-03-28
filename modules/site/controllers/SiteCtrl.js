9d9adf93da8a0dd20d20826842a6065af32f3ae3
githubjeka <et.coder@gmai.com>
2014-03-28 13:23:04 +0300

    upload

---
+++ modules/site/controllers/SiteCtrl.js
@@ -0,0 +1,43 @@
+app
+    .controller('SiteLogin', ['$scope', 'rest', 'toaster', '$window', function ($scope, rest, toaster, $window) {
+
+        rest.url = '/test/yii2/rest/user/login';
+
+        var errorCallback = function (data) {
+            toaster.clear();
+            delete $window.sessionStorage._auth;
+            angular.forEach(data, function (error) {
+                toaster.pop('error', "Field: " + error.field, error.message);
+            });
+        };
+
+        $scope.login = function () {
+            rest.postModel($scope.model).success(function (data) {
+                $window.sessionStorage._auth = data;
+                toaster.pop('success', "Success");
+                window.setTimeout(function () {
+                    document.location = '';
+                }, 1000);
+            }).error(errorCallback);
+        };
+
+        rest.postModel($scope.model).success(function (data) {
+            $window.sessionStorage._auth = data;
+            document.location = '';
+        });
+    }])
+    .controller('SiteLogout', ['$scope', 'rest', '$window', function ($scope, rest, $window) {
+
+        rest.url = '/test/yii2/rest/user/logout';
+
+        var errorCallback = function (data) {
+            console.log(data);
+        };
+
+        rest.get().success(function () {
+            delete $window.sessionStorage._auth;
+            document.location = '';
+        }).error(errorCallback);
+
+
+    }]);
